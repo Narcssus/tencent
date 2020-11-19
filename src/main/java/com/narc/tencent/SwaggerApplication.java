dbe61677b83107d18b41a5c91b65261eb4c0e892
Narcssus <narcssusinnook@163.com>
2020-11-19 16:08:33 +0800

    springcloud

--- src/main/java/com/narc/tencent/SwaggerApplication.java
+++ src/main/java/com/narc/tencent/SwaggerApplication.java
@@ -1,16 +1,16 @@
 package com.narc.tencent;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author : Narcssus
  * @date : 2019/11/17 17:14
  */
 @SpringBootApplication
-@Slf4j
+@EnableEurekaClient
 public class SwaggerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
